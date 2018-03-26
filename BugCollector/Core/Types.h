a38092bcbd0e39bd7d73d4efe7648476693d8743
Unknown <montify@gmail.com>
2018-03-26 19:45:13 +0200

    Auto stash before merge of "master" and "origin/master"
    Added I32 primitive Type

--- BugCollector/Core/Types.h
+++ BugCollector/Core/Types.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /*
-	Defines for all the platform types.
+Defines for all the platform types.
 */
 
 struct PlatformTypes
@@ -9,9 +9,10 @@ struct PlatformTypes
 	typedef float f32;
 	typedef unsigned char b8;
 	typedef unsigned int u32;
+	typedef int i32;
 };
 
 typedef PlatformTypes::f32 f32;
 typedef PlatformTypes::b8 b8;
 typedef PlatformTypes::u32 u32;
-
+typedef PlatformTypes::i32 i32;
