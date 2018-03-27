31f5e98c7d60966badc11008f9e84eaac76a1c3f
Unknown <montify@gmail.com>
2018-03-27 12:53:43 +0200

    Handle Resize (D3D11 Device)

--- BugCollector/Modules/DirectXAPI/CDirectXRenderTargetWindow.h
+++ BugCollector/Modules/DirectXAPI/CDirectXRenderTargetWindow.h
@@ -19,16 +19,16 @@ public:
 		createInfo.bEnableVerticalSync = true;
 		createInfo.Width = 1024;
 		createInfo.Height = 768;
-	
+
 		PlatformWindow.Initialize(createInfo);
 	}
 
-	
+
 	virtual void SetFullscreen() override
 	{
-	 
+
 	}
-	
+
 
 	virtual void SetFullscreenWindow() override
 	{
@@ -37,7 +37,7 @@ public:
 
 	virtual void SetWindowed(u32 InW, u32 InH) override
 	{
-		
+
 	}
 
 
@@ -48,6 +48,47 @@ public:
 
 	virtual void Resize(u32 InW, u32 InH) override
 	{
+		// Release all reference to the SwapChain
+		m_device->m_deviceContext->OMSetRenderTargets(0, 0, 0);
+		m_device->m_renderTargetView->Release();
+
+		HRESULT hr;
+
+		if (FAILED(hr = m_device->m_swapChain->ResizeBuffers(0, InW, InH, DXGI_FORMAT_UNKNOWN, 0)))
+		{
+			__debugbreak();
+		}
+
+
+		ID3D11Texture2D* pBuffer;
+		if (FAILED(hr = m_device->m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBuffer)))
+		{
+			__debugbreak();
+		}
+
+
+		if (FAILED(hr = m_device->m_nativeD3DDevice->CreateRenderTargetView(pBuffer, nullptr, &m_device->m_renderTargetView)))
+		{
+			__debugbreak();
+		}
+
+		pBuffer->Release();
+
+		m_device->m_deviceContext->OMSetRenderTargets(1, &m_device->m_renderTargetView, nullptr);
+		
+
+		// Set up the viewport.
+		D3D11_VIEWPORT vp;
+		vp.Width = InW;
+		vp.Height = InH;
+		vp.MinDepth = 0.0f;
+		vp.MaxDepth = 1.0f;
+		vp.TopLeftX = 0;
+		vp.TopLeftY = 0;
+
+		m_device->m_deviceContext->RSSetViewports(1, &vp);
+
+
 	}
 
 
