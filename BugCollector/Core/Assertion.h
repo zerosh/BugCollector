ad688e5675ae6a8fe69728ec0b1ae04ad200527c
Stian Hagen <neutroniac@gmail.com>
2018-03-29 01:19:14 +0200

    Check fix

--- BugCollector/Core/Assertion.h
+++ BugCollector/Core/Assertion.h
@@ -9,7 +9,7 @@
 	check if expression is true, otherwise it will fail and throw exception.
 	If you are inside a performance critical code use @Ref: check_slow, instead.
 */
-#define check(expression) { if (UNLIKE(!(expression))) { CDebug::LogAssert(#expression, __FILE__, __LINE__); __debugbreak();} } 
+#define check(expression) { if (UNLIKE(!(expression))) { CDebugManager::LogAssert(#expression, __FILE__, __LINE__); __debugbreak();} } 
 
 /*
 	Use this inside performance critical code look rendering loops, update loops etc
